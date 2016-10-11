11002e516b7c2180c673b06f04be296d7ae39b43
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-11 20:04:07 +0900

    10をメソッド化した

--- ruby/100knocks/10.rb
+++ ruby/100knocks/10.rb
@@ -2,4 +2,8 @@
 # 確認結果
 # ➜  100knocks git:(master) wc -l hightemp.txt
 #       24 hightemp.txt
-puts IO.readlines('hightemp.txt').count
+def count_row file_path
+  IO.readlines(file_path).count
+end
+
+puts count_row 'hightemp.txt'
