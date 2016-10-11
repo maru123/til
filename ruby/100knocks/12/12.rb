1b93a9706c2238170238ac05a3fea3fd00efcd25
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-11 20:15:05 +0900

    12を目ドッソ化した

--- ruby/100knocks/12/12.rb
+++ ruby/100knocks/12/12.rb
@@ -1,7 +1,11 @@
 # 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
 
+def extract_column read_file_path, column_number
+  File.write("./col#{column_number}.txt", IO.read(read_file_path).each_line.map{|line| line.split(/\t/)[column_number-1]}.join("\n"))
+end
+
 # 確認 cut -f1 ../hightemp.txtの結果と下記プログラムの結果が一致した
-File.write('./col1.txt', IO.read('../hightemp.txt').each_line.map{|line| line.split(/\t/)[0]}.join("\n"))
+extract_column '../hightemp.txt', 1
 
 # 確認 cut -f2 ../hightemp.txtの結果と下記プログラムの結果が一致した
-File.write('./col2.txt', IO.read('../hightemp.txt').each_line.map{|line| line.split(/\t/)[1]}.join("\n"))
+extract_column '../hightemp.txt', 2
