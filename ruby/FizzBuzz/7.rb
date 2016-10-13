5b8fe0cef6e6e7f880cc6fd8e59be70673665c38
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-13 20:13:00 +0900

    Proc化した

--- ruby/FizzBuzz/7.rb
+++ ruby/FizzBuzz/7.rb
@@ -1,1 +1,2 @@
-1.upto(100){|i| puts i%15 == 0 ? 'FizzBuzz' : i%3 == 0 ? 'Fizz' : i%5 == 0 ? 'Buzz' : i }
+proc = Proc.new{1.upto(100){|i| puts i%15 == 0 ? 'FizzBuzz' : i%3 == 0 ? 'Fizz' : i%5 == 0 ? 'Buzz' : i }}
+proc.call
