e10b10d1294b6beb730b3838f0ad4abbc2eaf3ab
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-13 20:11:16 +0900

    Proc化した

--- ruby/FizzBuzz/6.rb
+++ ruby/FizzBuzz/6.rb
@@ -1,2 +1,3 @@
 # puts を使わずに結果を返す
-[*1..100].map {|i| if i%15 == 0; 'FizzBuzz'; elsif i%3 == 0; 'Fizz'; elsif i%5 == 0; 'Buzz'; else i end}.join(' ')
+proc = Proc.new{[*1..100].map {|i| if i%15 == 0; 'FizzBuzz'; elsif i%3 == 0; 'Fizz'; elsif i%5 == 0; 'Buzz'; else i end}.join(' ')}
+proc.call
