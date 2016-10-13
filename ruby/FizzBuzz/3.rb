78b5165403ba6eaa0b28be961e36932da522fc68
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-13 19:56:48 +0900

    Proc化した

--- ruby/FizzBuzz/3.rb
+++ ruby/FizzBuzz/3.rb
@@ -1,2 +1,2 @@
 # メソッドにする
-def fizz_buzz num; (1..num).each_with_object([]) {|num, array| array << if num%15 == 0; 'FizzBuzz'; elsif num%3 == 0; 'Fizz'; elsif num%5 == 0; 'Buzz'; else num; end}.join(' '); end
+def fizz_buzz num; proc = Proc.new{(1..num).each_with_object([]) {|num, array| array << if num%15 == 0; 'FizzBuzz'; elsif num%3 == 0; 'Fizz'; elsif num%5 == 0; 'Buzz'; else num; end}.join(' ');}; proc.call end
