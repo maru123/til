2600350479f75dea547ee3874efa113b6726417a
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-13 20:07:49 +0900

    Proc化した

--- ruby/FizzBuzz/4.rb
+++ ruby/FizzBuzz/4.rb
@@ -1,2 +1,2 @@
 # if..else文を使わずに書く
-def fizz_buzz num; (1..num).each {|num| next puts 'FizzBuzz' if num%15 == 0; next puts 'Fizz' if num%3 == 0; next puts 'Buzz' if num%4 == 0; puts num; } end
+def fizz_buzz num; proc = Proc.new{(1..100).each {|num| next puts 'FizzBuzz' if num%15 == 0; next puts 'Fizz' if num%3 == 0; next puts 'Buzz' if num%4 == 0; puts num }}; proc.call; end
