b14acd83e150865b1e014615fe52df08ab6d6836
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-10-13 19:52:22 +0900

    Proc化した

--- ruby/FizzBuzz/1.rb
+++ ruby/FizzBuzz/1.rb
@@ -1,2 +1,3 @@
 # （１）n += 1を使わない
-for i in 1..100; text = ""; text = 'Fizz' if i%3 == 0; text += 'Buzz'if i%5 == 0; text = i if i%3 != 0 && i%5 != 0; puts text;end
+proc = Proc.new{ for i in 1..100; text = ""; text = 'Fizz' if i%3 == 0; text += 'Buzz'if i%5 == 0; text = i if i%3 != 0 && i%5 != 0; puts text;end}
+proc.call
